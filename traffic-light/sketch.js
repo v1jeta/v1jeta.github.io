12afe229b61a2d84ac81e0621f223c511a153d8a
vijeta <vthakur0507@gmail.com>
2024-09-25 10:24:55 -0600

    traffic light

--- traffic-light/sketch.js
+++ traffic-light/sketch.js
@@ -5,10 +5,12 @@
 // GOAL: make a 'traffic light' simulator. For now, just have the light
 // changing according to time. You may want to investigate the millis()
 // function at https://p5js.org/reference/#/p5/millis
-let lightState = 1
-let waitTime = 2000;
 
-
+let lightState = 1;
+let lastSwitchedTime = 0;
+const GREEN_LIGHT_DURATION = 3000;
+const YELLOW_LIGHT_DURATION = 1000;
+const RED_LIGHT_DURATION = 3500;
 
 function setup() {
   createCanvas(100, 300);
@@ -16,7 +18,10 @@ function setup() {
 
 function draw() {
   background(255);
-  drawOutlineOfLights();}
+  drawOutlineOfLights();
+  changeStateIfNeeded();
+  displayLight();
+}
 
 function drawOutlineOfLights() {
   //box
@@ -25,38 +30,38 @@ function drawOutlineOfLights() {
   rect(width / 2, height / 2, 75, 200, 10);
 
   //lights
+  fill(255);
+  ellipse(width / 2, height / 2 - 65, 50, 50); //top
+  ellipse(width / 2, height / 2, 50, 50); //middle
+  ellipse(width / 2, height / 2 + 65, 50, 50); //bottom
+}
+
+function displayLight(){
   if (lightState === 1){
-    fill('red');
-    ellipse(width / 2, height / 2 - 65, 50, 50); //top
-    lightState = 2;
+    fill("green");
+    ellipse(width / 2, height / 2 + 65, 50, 50); //bottom
+  }
   else if (lightState === 2){
     fill("yellow");
     ellipse(width / 2, height / 2, 50, 50); //middle
+  }
+  else if (lightState === 3){
+    fill("red");
+    ellipse(width / 2, height / 2 - 65, 50, 50); //top
+  }
+}
+
+function changeStateIfNeeded(){
+  if (lightState === 1 && millis()> lastSwitchedTime + GREEN_LIGHT_DURATION){
+    lightState = 2;
+    lastSwitchedTime = millis();
+  }
+  else if (lightState === 2 && millis()> lastSwitchedTime + YELLOW_LIGHT_DURATION){
     lightState = 3;
-
-  else{
-ellipse(width / 2, height / 2 + 65, 50, 50); //bottom
-}}
-
-
-
-// let waitTime = 1000;
-// let lastSwitchTime = 0;
-// let isWhite = true;
-
-// function setup() {
-//   createCanvas(600, 600);
-// }
-
-// function draw() {
-//   if (millis() > lastSwitchTime + waitTime) {
-//     isWhite = !isWhite;
-//     lastSwitchTime = millis();
-//   }
-  
-//   if (isWhite){
-//     background(255);
-//   } else {
-//     background(0);
-//   }
-// }
+    lastSwitchedTime = millis();
+  }
+  else if (lightState === 3 && millis()> lastSwitchedTime + RED_LIGHT_DURATION){
+    lightState = 1;
+    lastSwitchedTime = millis();
+  }
+}
