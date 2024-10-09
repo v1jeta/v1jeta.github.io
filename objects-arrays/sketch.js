108c3e6bd1db0f4d9ef36f41ec07804d43f51a48
vijeta <vthakur0507@gmail.com>
2024-10-09 11:02:51 -0600

    added different game states, started adding objects

--- objects-arrays/sketch.js
+++ objects-arrays/sketch.js
@@ -6,11 +6,32 @@
 // - describe what you did to take this project "above and beyond"
 
 // global
+let gameState = 0;
+let windowWidth = 1400; //adjust later to user resizing windows
+let windowHeight = 900;
+
+//objects 
+let apple = {
+  x: 200,
+  y: 100,
+  emoji: '🍎'
+};
+let banana = {
+  x:250,
+  y:100,
+  emoji: '🍌'
+};
+
+
+//media uploads
 let bgImage;
+let startScreenImg;
+
 
 // load images and audio
 function preload(){
   bgImage = loadImage('main-bg.png') ;
+  startScreenImg = loadImage('startscreen.jpeg');
 }
 
 function setup() {
@@ -19,5 +40,28 @@ function setup() {
 
 function draw() {
   background(220);
-  image(bgImage,width/2, height/2);
+  displayScreens();
+}
+
+function displayScreens(){
+  if (gameState === 0){
+    //start screen
+    image(startScreenImg, 0, 0);
+    textSize(25);
+    text(apple.emoji, apple.x, apple.y);
+    text(banana.emoji,banana.x,banana.y);
+  }
+
+  if (gameState === 1){
+    //game running
+    image(bgImage, width/2, height/2);
+  }
+
+  if (gameState === 2){
+    //win screen
+  }
+
+  if (gameState === 3){
+    //lose screen
+  }
 }
